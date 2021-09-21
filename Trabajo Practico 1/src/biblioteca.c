b7d1f79154a16fd0858a2ebf624e779e5de68c84
Thiago <mejiasthiago1@gmail.com>
2021-09-21 18:48:44 -0300

    proyecto funcionando

---
+++ Trabajo Practico 1/src/biblioteca.c
@@ -0,0 +1,47 @@
+#include "biblioteca.h"
+float suma(float numero1, float numero2) {
+	float resultado;
+	resultado = numero1 + numero2;
+	return resultado;
+}
+float resta(float numero1, float numero2) {
+	float resultado;
+	resultado = numero1 - numero2;
+	return resultado;
+
+}
+
+float division(float numero1, float numero2) {
+	float resultado;
+	resultado = 0;
+	if (numero2 != 0) {
+		resultado = numero1 / numero2;
+	}
+	return resultado;
+}
+
+float multiplicacion(float numero1, float numero2) {
+	float resultado;
+	resultado = numero1 * numero2;
+	return resultado;
+}
+
+float factorizacion(float numero1) {
+	float resultado = -1;
+
+	if (numero1 - (int) numero1 == 0 && numero1 > -1) {
+		float multiplicador;
+
+		multiplicador = numero1 - 1;
+
+		while (multiplicador > 1) {
+			numero1 = numero1 * multiplicador;
+			multiplicador--;
+		}
+
+		return numero1;
+	}
+
+	return resultado;
+}
+
